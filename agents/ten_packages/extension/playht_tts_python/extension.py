a04b35c38dd844e83ee0828cfb4b97ba9496b055
seymourtang <tangxianmeng@live.com>
2025-01-13 20:26:13 +0800

    feat:Implement of playht extension

--- agents/ten_packages/extension/playht_tts_python/extension.py
+++ agents/ten_packages/extension/playht_tts_python/extension.py
@@ -5,7 +5,9 @@
 #
 import asyncio
 import time
-from .playht_tts import PlayhtTTS, PlayhtTTSConfig
+
+from ten_ai_base.types import TTSPcmOptions
+from .playht_tts import SAMPLE_RATE, PlayhtTTS, PlayhtTTSConfig
 from ten import (
     AsyncTenEnv,
 )
@@ -20,20 +22,22 @@ class Addon(AsyncTTSBaseExtension):
 
     async def on_init(self, ten_env: AsyncTenEnv) -> None:
         await super().on_init(ten_env)
-        ten_env.log_debug("on_init")
+        ten_env.log_info("on_init")
 
     async def on_start(self, ten_env: AsyncTenEnv) -> None:
         await super().on_start(ten_env)
-        ten_env.log_debug("on_start")
+        ten_env.log_info("on_start")
 
         self.config = await PlayhtTTSConfig.create_async(ten_env=ten_env)
         self.client = PlayhtTTS(self.config)
+        self.client.create_synthesizer(ten_env)
 
+        asyncio.create_task(self.mock_on_request_text(ten_env))
         asyncio.create_task(self._process_audio_data(ten_env))
 
     async def on_stop(self, ten_env: AsyncTenEnv) -> None:
         await super().on_stop(ten_env)
-        ten_env.log_debug("on_stop")
+        ten_env.log_info("on_stop")
 
         await self.queue.put(None)
 
@@ -48,7 +52,8 @@ class Addon(AsyncTTSBaseExtension):
             if audio_data is None:
                 break
 
-            await self.send_audio_out(ten_env, audio_data)
+            additional_args = {"sample_rate": SAMPLE_RATE}
+            await self.send_audio_out(ten_env, audio_data, **additional_args)
 
     async def on_request_tts(
         self, ten_env: AsyncTenEnv, input_text: str, end_of_segment: bool
@@ -56,7 +61,7 @@ class Addon(AsyncTTSBaseExtension):
         ten_env.log_info(
             rf"on_request_tts: {input_text},TTS_TEST_POINT_SEND:{int(time.time() * 1000)}"
         )
-        self.client.text_to_speech_stream(ten_env, input_text, end_of_segment)
+        await self.client.text_to_speech_stream(ten_env, input_text, end_of_segment)
 
     async def on_cancel_tts(self, ten_env: AsyncTenEnv) -> None:
         self.client.cancel(ten_env)
--- agents/ten_packages/extension/playht_tts_python/playht_tts.py
+++ agents/ten_packages/extension/playht_tts_python/playht_tts.py
@@ -6,68 +6,146 @@ from websocket import WebSocketConnectionClosedException
 
 from ten.async_ten_env import AsyncTenEnv
 from ten_ai_base.config import BaseConfig
-import dashscope
-from dashscope.audio.tts_v2 import SpeechSynthesizer, AudioFormat, ResultCallback
+from typing import AsyncGenerator, AsyncIterable, AsyncIterator, Union
+from pyht import AsyncClient, Format
+from pyht.client import TTSOptions
+import numpy as np
+
+
+def convert_f32le_to_s16le_stream(input_bytes):
+    pcm_f32_data = np.frombuffer(input_bytes, dtype=np.float32)
+
+    pcm_s16_data = np.clip(pcm_f32_data * 32768, -32768, 32767).astype(np.int16)
+
+    output_bytes = pcm_s16_data.tobytes()
+
+    return bytearray(output_bytes)
 
 
 @dataclass
 class PlayhtTTSConfig(BaseConfig):
     user_id: str = ""
     api_key: str = ""
-    voice: str = ""
-    sample_rate: int = 16000
+    # reference to https://docs.play.ht/reference/list-of-prebuilt-voices
+    voice: str = (
+        "s3://voice-cloning-zero-shot/775ae416-49bb-4fb6-bd45-740f205d20a1/jennifersaad/manifest.json"
+    )
+
+
+SAMPLE_RATE = 48000
+
+
+class PlayhtTTSClientHandler:
+    def __init__(
+        self,
+        ten_env: AsyncTenEnv,
+        queue: asyncio.Queue,
+        in_stream: AsyncIterator[str],
+        out_stream: Union[AsyncGenerator[bytes, None], AsyncIterable[bytes]],
+    ) -> None:
+        self.queue = queue
+        self.ten_env = ten_env
+        self.in_stream = in_stream
+        self.out_stream = out_stream
+        self.buffer = bytearray()
+
+    def start(self) -> None:
+        asyncio.create_task(self._handle_audio_data())
+
+    async def send_text_to_speech(self, text: str) -> None:
+        if not self.in_stream:
+            return
+        self.ten_env.log_info(f"Sending text to speech: {text}")
+        await self.in_stream(text)
+
+    async def _handle_audio_data(self) -> None:
+        self.ten_env.log_info("Handling audio data")
+        if not self.out_stream:
+            self.ten_env.log_error("No out stream")
+            return
+        async for chunk in self.out_stream:
+            self.ten_env.log_info(
+                f"Received pcm data: {len(chunk)} bytes,TTS_TEST_POINT_RECEIVED:{int(time.time() * 1000)}"
+            )
+            # Convert audio data from float32 format to int16 format, ensuring the input chunk is a multiple of 4.
+            # Any extra data will be saved to the buffer.
+            # First, merge the data in the buffer with the new data, then perform the conversion.
+            data = self.buffer + chunk
+            self.buffer = bytearray()
+            if len(data) % 4 != 0:
+                valid_length = len(data) - (len(data) % 4)
+                self.buffer = data[valid_length:]
+                data = data[:valid_length]
+
+            await self.queue.put(convert_f32le_to_s16le_stream(data))
+        self.ten_env.log_info("Audio data handling done")
+
+    def close(self) -> None:
+        self.ten_env.log_info("Closing handler")
+        if self.out_stream:
+            self.out_stream.close()
+
 
 class PlayhtTTS:
     def __init__(self, config: PlayhtTTSConfig) -> None:
         self.config = config
-        self.synthesizer = None  # Initially no synthesizer
+        self.client = None  # Initially no synthesizer
         self.queue = asyncio.Queue()
+        self.tts_options = None
+        self.handler = None
 
-    def _create_synthesizer(
-        self, ten_env: AsyncTenEnv, callback: AsyncIteratorCallback
-    ):
-        if self.synthesizer:
-            self.synthesizer = None
+    def create_synthesizer(self, ten_env: AsyncTenEnv):
+        if self.client:
+            self.client = None
 
         ten_env.log_info("Creating new synthesizer")
-        self.synthesizer = SpeechSynthesizer(
-            model=self.config.model,
-            voice=self.config.voice,
-            format=AudioFormat.PCM_16000HZ_MONO_16BIT,
-            callback=callback,
+        self.client = AsyncClient(
+            user_id=self.config.user_id,
+            api_key=self.config.api_key,
         )
+        ten_env.log_info(f"Config:{self.config}")
+        self.tts_options = TTSOptions(
+            voice=self.config.voice,
+            format=Format.FORMAT_RAW,
+            sample_rate=SAMPLE_RATE,
+        )
+        in_stream, out_stream = self.client.get_stream_pair(
+            self.tts_options, voice_engine="Play3.0-mini-ws"
+        )
+        self.handler = PlayhtTTSClientHandler(
+            ten_env, self.queue, in_stream, out_stream
+        )
+        self.handler.start()
 
     async def get_audio_bytes(self) -> bytes:
         return await self.queue.get()
 
-    def text_to_speech_stream(
+    async def text_to_speech_stream(
         self, ten_env: AsyncTenEnv, text: str, end_of_segment: bool
     ) -> None:
         try:
-            callback = AsyncIteratorCallback(ten_env, self.queue)
+            if not self.client:
+                self.create_synthesizer(ten_env)
 
-            if not self.synthesizer or end_of_segment:
-                self._create_synthesizer(ten_env, callback)
+            await self.handler.send_text_to_speech(text)
 
-            self.synthesizer.streaming_call(text)
-
-            if end_of_segment:
-                ten_env.log_info("Streaming complete")
-                self.synthesizer.streaming_complete()
-                self.synthesizer = None
         except WebSocketConnectionClosedException as e:
             ten_env.log_error(f"WebSocket connection closed, {e}")
-            self.synthesizer = None
+            self.client = None
         except Exception as e:
             ten_env.log_error(f"Error streaming text, {e}")
-            self.synthesizer = None
+            self.client = None
 
     def cancel(self, ten_env: AsyncTenEnv) -> None:
-        if self.synthesizer:
+        if self.client:
             try:
-                self.synthesizer.streaming_cancel()
+                self.out_stream.close()
+                self.out_stream = None
             except WebSocketConnectionClosedException as e:
                 ten_env.log_error(f"WebSocket connection closed, {e}")
             except Exception as e:
                 ten_env.log_error(f"Error cancelling streaming, {e}")
-            self.synthesizer = None
+            self.handler.close()
+            self.client.close()
+            self.handler = None
+            self.client = None
